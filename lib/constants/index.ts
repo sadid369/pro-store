5359d2b4acc2016da209fcaf8f72688f0b75574a
Sadid <66120003+sadid369@users.noreply.github.com>
2025-04-16 20:12:43 +0600

    check

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "ProStore";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "ProStores";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A  Modern Ecommerce Store";
 export const SERVER_URL =
